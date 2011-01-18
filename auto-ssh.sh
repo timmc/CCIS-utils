9e4fa204ff4abd98bc07f0e93ce1a9e949f3dd87
Tim McCormack <cortex@brainonfire.net>
2011-01-17 22:32:02 -0500

    * Use default keypair name. * Bail if already there.

--- auto-ssh.sh
+++ auto-ssh.sh
@@ -2,14 +2,18 @@
 # Create an SSH keypair with for connecting from one CCIS machine to another.
 # The passphrase is empty, and the public key will be authorized for you.
 
-KEY_FNAME="id_CCIS_auto"
 COMMENT="`whoami`@CCIS/auto"
 
 set -o errexit
 set -o nounset
 
-ssh-keygen -q -N "" -f ~/.ssh/$KEY_FNAME -t rsa -b 4096 -C "$COMMENT"
-cat ~/.ssh/$KEY_FNAME.pub >> ~/.ssh/authorized_keys
+if [ -f ~/.ssh/id_rsa.pub ] ; then
+  echo You already have an SSH keypair. Bailing out, you can do this yourself.
+  exit 1
+fi
 
-echo "Finished. You can now do ssh between CCIS machines without interaction."
+ssh-keygen -q -N "" -f ~/.ssh/id_rsa -t rsa -b 4096 -C "$COMMENT"
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+
+echo Finished. You can now do ssh between CCIS machines without interaction.
 
