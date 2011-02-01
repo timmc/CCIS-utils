d8bfcce9c5f1ef0267582c13257224cfcdd7e65c
Tim McCormack <cortex@brainonfire.net>
2011-01-31 22:01:26 -0500

    Show uptimes in days of lab computers.

---
+++ uptime.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Suggested usage: ./uptime.sh | sort -rg | head -n 20
+
+FILT_UPTIME_1="sed 's:.*[^0-9]\([0-9]\+\) day.*:\1:'"
+FILT_UPTIME_2="sed 's|.*[^0-9]\([0-9]\+\):[0-9]\+:[0-9]\+ up.*|\1|'"
+
+for x in `/ccis/bin/linuxmachines 2>&1 | grep -v virtual | grep -v account. | grep ccs.neu.edu | sed 's/^\([a-z0-9]\+\).*/\1/'`
+do
+   days=`ssh -o NumberOfPasswordPrompts=0 -o StrictHostKeyChecking=false $x "uptime | $FILT_UPTIME_1 | $FILT_UPTIME_2" 2>/dev/null`
+   SSH_EXIT="$?"
+   if [ "$SSH_EXIT" -eq "0" ] ; then
+     echo "$days $x"
+   fi
+done
+
