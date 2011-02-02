cbbb40991987de42b2392aa19177ea60d2dde73c
Tim McCormack <cortex@brainonfire.net>
2011-02-01 21:47:54 -0500

    Don't suppress STDOUT

--- uptime.sh
+++ uptime.sh
@@ -7,7 +7,7 @@ FILT_UPTIME_2="sed 's|.*[^0-9]\([0-9]\+\):[0-9]\+:[0-9]\+ up.*|\1|'"
 
 for x in `/ccis/bin/linuxmachines 2>&1 | grep -v virtual | grep -v account. | grep ccs.neu.edu | sed 's/^\([a-z0-9]\+\).*/\1/'`
 do
-   days=`ssh -o NumberOfPasswordPrompts=0 -o StrictHostKeyChecking=false $x "uptime | $FILT_UPTIME_1 | $FILT_UPTIME_2" 2>/dev/null`
+   days=`ssh -o NumberOfPasswordPrompts=0 -o StrictHostKeyChecking=false $x "uptime | $FILT_UPTIME_1 | $FILT_UPTIME_2"`
    SSH_EXIT="$?"
    if [ "$SSH_EXIT" -eq "0" ] ; then
      echo "$days $x"
