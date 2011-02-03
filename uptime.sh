a607df7ef9f5e2cdc019e449cdad75383c9e9a1a
Tim McCormack <cortex@brainonfire.net>
2011-02-03 10:56:17 -0500

    Prefilter uptime results to add '0 days', force regex to find days field

--- uptime.sh
+++ uptime.sh
@@ -2,12 +2,13 @@
 # Requires non-interactive SSH auth between CCIS machines, such as that provided by auto-ssh.sh
 # Suggested usage: ./uptime.sh | sort -rg | head -n 20
 
-FILT_UPTIME_1="sed 's:.*[^0-9]\([0-9]\+\) day.*:\1:'"
-FILT_UPTIME_2="sed 's|.*[^0-9]\([0-9]\+\):[0-9]\+:[0-9]\+ up.*|\1|'"
+FILT_UPTIME_PRE="sed 's|:\([0-9]\+\) up \([0-9]\+\):|:\1 up 0 days, \2:|'"
+FILT_UPTIME_1="sed 's|.*[^0-9]\([0-9]\+\) day.*|\1|'"
+FILT_UPTIME_2="sed 's|.*up \([0-9]\+\) day.*|\1|'"
 
 for x in `/ccis/bin/linuxmachines 2>&1 | grep -v virtual | grep -v account. | grep ccs.neu.edu | sed 's/^\([a-z0-9]\+\).*/\1/'`
 do
-   days=`ssh -o NumberOfPasswordPrompts=0 -o StrictHostKeyChecking=false $x "uptime | $FILT_UPTIME_1 | $FILT_UPTIME_2"`
+   days=`ssh -o NumberOfPasswordPrompts=0 -o StrictHostKeyChecking=false $x "uptime | $FILT_UPTIME_PRE | $FILT_UPTIME_1 | $FILT_UPTIME_2"`
    SSH_EXIT="$?"
    if [ "$SSH_EXIT" -eq "0" ] ; then
      echo "$days $x"
